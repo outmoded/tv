6e7e49bd4d0205b82298b9f061f683b686ebd0c7
Eran Hammer <eran@hueniverse.com>
2014-03-22 10:54:02 -0700

    Coverage. Closes #66

--- test/index.js
+++ test/index.js
@@ -21,15 +21,9 @@ var it = Lab.test;
 
 describe('Tv', function () {
 
-    var server = null;
+    it('reports a request event', function (done) {
 
-    before(function (done) {
-
-        var options = {
-            port: 0
-        };
-
-        server = new Hapi.Server(0);
+        var server = new Hapi.Server(0);
 
         server.route({
             method: 'GET',
@@ -40,42 +34,58 @@ describe('Tv', function () {
             }
         });
 
-        server.pack.require('../', options, function (err) {
+        server.pack.require('../', { port: 0 }, function (err) {
 
             expect(err).to.not.exist;
-            done();
+
+            server.inject('/debug/console', function (res) {
+
+                expect(res.statusCode).to.equal(200);
+                expect(res.result).to.contain('Debug Console');
+
+                var host = res.result.match(/var host = '([^']+)'/)[1];
+                var port = res.result.match(/var port = (\d+)/)[1];
+                var ws = new Ws('ws://' + host + ':' + port);
+
+                ws.once('open', function () {
+
+                    ws.send('*');
+
+                    setTimeout(function () {
+
+                        server.inject('/?debug=123', function (res) {
+
+                            expect(res.result).to.equal('1');
+                        });
+                    }, 100);
+                });
+
+                ws.once('message', function (data, flags) {
+
+                    expect(JSON.parse(data).data.agent).to.equal('shot');
+                    done();
+                });
+            });
         });
     });
 
-    it('reports a request event', function (done) {
+    it('uses specified hostname', function (done) {
 
-        server.inject('/debug/console', function (res) {
+        var server = new Hapi.Server(0);
 
-            expect(res.statusCode).to.equal(200);
-            expect(res.result).to.contain('Debug Console');
+        server.route({
+            method: 'GET',
+            path: '/',
+            handler: function (request, reply) {
 
-            var host = res.result.match(/var host = '([^']+)'/)[1];
-            var port = res.result.match(/var port = (\d+)/)[1];
-            var ws = new Ws('ws://' + host + ':' + port);
+                reply('1');
+            }
+        });
 
-            ws.once('open', function () {
+        server.pack.require('../', { host: '127.0.0.1', port: 0 }, function (err) {
 
-                ws.send('*');
-
-                setTimeout(function () {
-
-                    server.inject('/?debug=123', function (res) {
-
-                        expect(res.result).to.equal('1');
-                    });
-                }, 100);
-            });
-
-            ws.once('message', function (data, flags) {
-
-                expect(JSON.parse(data).data.agent).to.equal('shot');
-                done();
-            });
+            expect(err).to.not.exist;
+            done();
         });
     });
 });
