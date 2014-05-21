56a4733d47066b5e8f61ae0d608dc46704b97239
Wyatt Preul <wpreul@gmail.com>
2014-05-21 12:29:22 -0500

    Upgrading to hapi 5

--- test/index.js
+++ test/index.js
@@ -69,6 +69,61 @@ describe('Tv', function () {
         });
     });
 
+    it('handles reconnects gracefully', function (done) {
+
+        var server = new Hapi.Server(0);
+
+        server.route({
+            method: 'GET',
+            path: '/',
+            handler: function (request, reply) {
+
+                reply('1');
+            }
+        });
+
+        server.pack.require('../', { port: 0, host: 'localhost' }, function (err) {
+
+            expect(err).to.not.exist;
+
+            server.inject('/debug/console', function (res) {
+
+                expect(res.statusCode).to.equal(200);
+                expect(res.result).to.contain('Debug Console');
+
+                var host = res.result.match(/var host = '([^']+)'/)[1];
+                var port = res.result.match(/var port = (\d+)/)[1];
+                var ws1 = new Ws('ws://' + host + ':' + port);
+
+                ws1.once('open', function () {
+
+                    ws1.send('*');
+                    ws1.close();
+                    var ws2 = new Ws('ws://' + host + ':' + port);
+
+                    ws2.once('open', function () {
+
+                        ws2.send('*');
+                        setTimeout(function () {
+
+                            server.inject('/?debug=123', function (res) {
+
+                                expect(res.result).to.equal('1');
+                            });
+                        }, 100);
+                    });
+
+                    // Shouldn't get called
+                    ws2.once('message', function (data, flags) {
+
+                        expect(JSON.parse(data).data.agent).to.equal('shot');
+                        done();
+                    });
+                });
+            });
+        });
+    });
+
     it('uses specified hostname', function (done) {
 
         var server = new Hapi.Server(0);
