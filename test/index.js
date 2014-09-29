1a2b1c7f5526fe6d50e075b9626d73f91ea29912
Adam Bretz <arbretz@gmail.com>
2014-09-29 12:09:52 -0400

    Cleaned up tests.

--- test/index.js
+++ test/index.js
@@ -19,38 +19,89 @@ var it = lab.it;
 var expect = Lab.expect;
 
 
-describe('Tv', function () {
+it('reports a request event', function (done) {
 
-    it('reports a request event', function (done) {
+    var server = new Hapi.Server(0);
 
-        var server = new Hapi.Server(0);
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
 
-        server.route({
-            method: 'GET',
-            path: '/',
-            handler: function (request, reply) {
+            reply('1');
+        }
+    });
 
-                reply('1');
-            }
+    server.pack.register({ plugin: Tv, options: { port: 0 } }, function (err) {
+
+        expect(err).to.not.exist;
+
+        server.inject('/debug/console', function (res) {
+
+            expect(res.statusCode).to.equal(200);
+            expect(res.result).to.contain('Debug Console');
+
+            var host = res.result.match(/var host = '([^']+)'/)[1];
+            var port = res.result.match(/var port = (\d+)/)[1];
+            var ws = new Ws('ws://' + host + ':' + port);
+
+            ws.once('open', function () {
+
+                ws.send('*');
+
+                setTimeout(function () {
+
+                    server.inject('/?debug=123', function (res) {
+
+                        expect(res.result).to.equal('1');
+                    });
+                }, 100);
+            });
+
+            ws.once('message', function (data, flags) {
+
+                expect(JSON.parse(data).data.agent).to.equal('shot');
+                done();
+            });
         });
+    });
+});
 
-        server.pack.register({ plugin: Tv, options: { port: 0 } }, function (err) {
+it('handles reconnects gracefully', function (done) {
 
-            expect(err).to.not.exist;
+    var server = new Hapi.Server(0);
 
-            server.inject('/debug/console', function (res) {
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
 
-                expect(res.statusCode).to.equal(200);
-                expect(res.result).to.contain('Debug Console');
+            reply('1');
+        }
+    });
 
-                var host = res.result.match(/var host = '([^']+)'/)[1];
-                var port = res.result.match(/var port = (\d+)/)[1];
-                var ws = new Ws('ws://' + host + ':' + port);
+    server.pack.register({ plugin: Tv, options: { port: 0, host: 'localhost' }}, function (err) {
 
-                ws.once('open', function () {
+        expect(err).to.not.exist;
 
-                    ws.send('*');
+        server.inject('/debug/console', function (res) {
 
+            expect(res.statusCode).to.equal(200);
+            expect(res.result).to.contain('Debug Console');
+
+            var host = res.result.match(/var host = '([^']+)'/)[1];
+            var port = res.result.match(/var port = (\d+)/)[1];
+            var ws1 = new Ws('ws://' + host + ':' + port);
+
+            ws1.once('open', function () {
+
+                ws1.send('*');
+                ws1.close();
+                var ws2 = new Ws('ws://' + host + ':' + port);
+
+                ws2.once('open', function () {
+
+                    ws2.send('*');
                     setTimeout(function () {
 
                         server.inject('/?debug=123', function (res) {
@@ -60,7 +111,8 @@ describe('Tv', function () {
                     }, 100);
                 });
 
-                ws.once('message', function (data, flags) {
+                // Shouldn't get called
+                ws2.once('message', function (data, flags) {
 
                     expect(JSON.parse(data).data.agent).to.equal('shot');
                     done();
@@ -68,112 +120,55 @@ describe('Tv', function () {
             });
         });
     });
+});
 
-    it('handles reconnects gracefully', function (done) {
+it('uses specified hostname', function (done) {
 
-        var server = new Hapi.Server(0);
+    var server = new Hapi.Server(0);
 
-        server.route({
-            method: 'GET',
-            path: '/',
-            handler: function (request, reply) {
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
 
-                reply('1');
-            }
-        });
-
-        server.pack.register({ plugin: Tv, options: { port: 0, host: 'localhost' }}, function (err) {
-
-            expect(err).to.not.exist;
-
-            server.inject('/debug/console', function (res) {
-
-                expect(res.statusCode).to.equal(200);
-                expect(res.result).to.contain('Debug Console');
-
-                var host = res.result.match(/var host = '([^']+)'/)[1];
-                var port = res.result.match(/var port = (\d+)/)[1];
-                var ws1 = new Ws('ws://' + host + ':' + port);
-
-                ws1.once('open', function () {
-
-                    ws1.send('*');
-                    ws1.close();
-                    var ws2 = new Ws('ws://' + host + ':' + port);
-
-                    ws2.once('open', function () {
-
-                        ws2.send('*');
-                        setTimeout(function () {
-
-                            server.inject('/?debug=123', function (res) {
-
-                                expect(res.result).to.equal('1');
-                            });
-                        }, 100);
-                    });
-
-                    // Shouldn't get called
-                    ws2.once('message', function (data, flags) {
-
-                        expect(JSON.parse(data).data.agent).to.equal('shot');
-                        done();
-                    });
-                });
-            });
-        });
+            reply('1');
+        }
     });
 
-    it('uses specified hostname', function (done) {
+    server.pack.register({plugin: Tv, options: { host: '127.0.0.1', port: 0 } }, function (err) {
 
-        var server = new Hapi.Server(0);
-
-        server.route({
-            method: 'GET',
-            path: '/',
-            handler: function (request, reply) {
-
-                reply('1');
-            }
-        });
-
-        server.pack.register({plugin: Tv, options: { host: '127.0.0.1', port: 0 } }, function (err) {
-
-            expect(err).to.not.exist;
-            done();
-        });
-    });
-
-    it('uses specified public hostname', function (done) {
-
-        var server = new Hapi.Server(0);
-
-        server.route({
-            method: 'GET',
-            path: '/',
-            handler: function (request, reply) {
-
-                reply('1');
-            }
-        });
-
-        server.pack.register({ plugin: Tv, options: { port: 0, host: 'localhost', publicHost: '127.0.0.1' }}, function (err) {
-
-            expect(err).to.not.exist;
-
-            server.inject('/debug/console', function (res) {
-
-                expect(res.statusCode).to.equal(200);
-                expect(res.result).to.contain('Debug Console');
-
-                var host = res.result.match(/var host = '([^']+)'/)[1];
-
-                expect(host).to.equal('127.0.0.1');
-                done();
-
-            });
-        });
+        expect(err).to.not.exist;
+        done();
     });
 });
 
+it('uses specified public hostname', function (done) {
 
+    var server = new Hapi.Server(0);
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+
+            reply('1');
+        }
+    });
+
+    server.pack.register({ plugin: Tv, options: { port: 0, host: 'localhost', publicHost: '127.0.0.1' }}, function (err) {
+
+        expect(err).to.not.exist;
+
+        server.inject('/debug/console', function (res) {
+
+            expect(res.statusCode).to.equal(200);
+            expect(res.result).to.contain('Debug Console');
+
+            var host = res.result.match(/var host = '([^']+)'/)[1];
+
+            expect(host).to.equal('127.0.0.1');
+            done();
+
+        });
+    });
+});
