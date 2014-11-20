bd15129f5aa5eebbce6c06a23dbabe8cfb86d290
Michael Phillips <mlp.brooklyn@gmail.com>
2014-11-20 13:55:23 -0700

    Setup styles for requests/server log rows.

--- source/js/views/components/app.js
+++ source/js/views/components/app.js
@@ -5,16 +5,13 @@ var FeedComponent = require('./feed');
 var AppComponent = React.createClass({
 
   getInitialState: function() { 
-    return {
-      requests: [], 
-      serverLogs: [] 
-    }
+    return { requests: [] }
   },
 
   render: function() {
     return (
       <div>
-        <FeedComponent requests={this.state.requests} serverLogs={this.state.serverLogs}/>
+        <FeedComponent requests={this.state.requests}/>
       </div>
     );
   }
--- source/js/views/components/feed.js
+++ source/js/views/components/feed.js
@@ -4,7 +4,26 @@ var _ = require('lodash');
 
 var FeedComponent = React.createClass({
 
-  toggle: function(e) {
+  render: function() {
+    return (
+      <table className="table">
+        <thead>
+          <th>Path</th>
+          <th>Method</th>
+          <th>Status</th>
+          <th>Data</th>
+          <th>Timestamp</th>
+        </thead>
+        <tbody>
+          {
+            this.props.requests.map(this._requestGroup)
+          }
+        </tbody>
+      </table>
+    );
+  },
+
+  _toggle: function(e) {
     var $requestRow = $(e.currentTarget);
 
     var $current = $requestRow.next();
@@ -17,61 +36,46 @@ var FeedComponent = React.createClass({
     var hide = !serverLogRows[0].hasClass('hidden');
 
     if(hide) {
-      _.each(serverLogRows, function($row){ $row.addClass('hidden'); });
+      $requestRow.removeClass('active');
+      _.each(serverLogRows, function($row) { $row.addClass('hidden'); });
     } else {
-      _.each(serverLogRows, function($row){ $row.removeClass('hidden'); });
+      $requestRow.addClass('active');
+      _.each(serverLogRows, function($row) { $row.removeClass('hidden'); });
     }
   },
 
-  render: function() {
-    var requestRow = function(request, stripe) {
-      var statusCodeContent = request.statusCode ? request.statusCode : <div className="spinner"></div>;
+  _requestGroup: function(request, index) {
+    var stripe = index % 2 === 0 ? 'dark' : 'light';
 
-      return (
-        <tr className={"request " + stripe} onClick={this.toggle}>
-          <td>{request.path}</td>
-          <td className="method">{request.method}</td>
-          <td>{statusCodeContent}</td>
-          <td>{JSON.stringify(request.data)}</td>
-          <td>{request.timestamp}</td>
-        </tr>
-      );
-    }.bind(this);
+    var requestRowMarkup = this._requestRow(request, stripe);
+    var serverLogRowsMarkup = request.serverLogs.map(function(serverLog) {
+      return this._serverLogRow(serverLog, stripe);
+    }.bind(this));
 
-    var serverLogRow = function(serverLog, stripe) {
-      return (
-        <tr className={"server-log hidden " + stripe}>
-          <td colSpan="3">{serverLog.tags.join(', ')}</td>
-          <td>{serverLog.data}</td>
-          <td>{serverLog.timestamp}</td>
-        </tr>
-      );
-    }.bind(this);
+    return _.flatten([requestRowMarkup, serverLogRowsMarkup], true);
+  },
+
+  _requestRow: function(request, stripe) {
+    var statusCodeContent = request.statusCode ? request.statusCode : <div className="spinner"></div>;
 
     return (
-      <table className="table">
-        <thead>
-          <th>Path</th>
-          <th>Method</th>
-          <th>Status</th>
-          <th>Data</th>
-          <th>Timestamp</th>
-        </thead>
-        <tbody>
-          {
-            this.props.requests.map(function(request, index) {
-              var stripe = index % 2 === 0 ? 'dark' : 'light';
+      <tr className={"request " + stripe} onClick={this._toggle}>
+        <td>{request.path}</td>
+        <td className="method">{request.method}</td>
+        <td>{statusCodeContent}</td>
+        <td>{JSON.stringify(request.data)}</td>
+        <td>{request.timestamp}</td>
+      </tr>
+    );
+  },
 
-              var requestRowMarkup = requestRow(request, stripe);
-              var serverLogRowsMarkup = request.serverLogs.map(function(serverLog) {
-                return serverLogRow(serverLog, stripe);
-              });
-
-              return _.flatten([requestRowMarkup, serverLogRowsMarkup], true);
-            })
-          }
-        </tbody>
-      </table>
+  _serverLogRow: function(serverLog, stripe) {
+    return (
+      <tr className={"server-log hidden " + stripe}>
+        <td colSpan="3">{serverLog.tags.join(', ')}</td>
+        <td>{serverLog.data}</td>
+        <td>{serverLog.timestamp}</td>
+      </tr>
     );
   }
 
