e5b03a501aceaed864f38ce3f1fc14f30bd450fb
Michael Phillips <mlp.brooklyn@gmail.com>
2014-11-25 13:42:35 -0700

    Rewrite to fit Hapi style

--- source/js/clientId.js
+++ source/js/clientId.js
@@ -16,81 +16,38 @@ internals.numbers = '0123456789';
 internals.localStorageKey = 'clientId';
 
 
-var createClientId = function(options) {
-    return 
-}
 
-var ClientId = exports = module.exports = {
 
-    install: function(options) {
-        var clientId, existingClientId = this._store.get(internals.localStorageKey);
-        
-        if (!existingClientId) {
-            clientId = this._store.set(internals.localStorageKey, this._generateClientId(options));
-        }
-        
-        return clientId;
-    },
+exports = module.exports = internals.ClientId = {};
 
-    _store: {
-        get: function(key) {
-            return localStorage.getItem(key);
-        },
-        set: function(key, value) {
-            localStorage.setItem(key, value);
-            return value;
-        }
-    },
-
-    _generateClientId: function(options) {
-        options = _.defaults(options || {}, internals.defaults);
-
-        var possible = (options.letters ? internals.letters : '') + (options.numbers ? internals.numbers : '');
-
-        return _.times(options.length, function() {
-            return possible.charAt(Math.floor(Math.random() * possible.length));
-        }).join('');
-    },
-
-    defaults: internals.defaults
-
+internals.ClientId.install = function(options) {
+    var clientId, existingClientId = this._store.get(internals.localStorageKey);
+    
+    if (!existingClientId) {
+        clientId = this._store.set(internals.localStorageKey, this._generateClientId(options));
+    }
+    
+    return clientId;
 };
 
+internals.ClientId._store = {
+    get: function(key) {
+        return localStorage.getItem(key);
+    },
+    set: function(key, value) {
+        localStorage.setItem(key, value);
+        return value;
+    }
+};
 
-// exports = module.exports = internals.ClientId = function(options) {
-//     options = _.defaults(options || {}, internals.defaults);
-    
-//     return _generateClientId(options);
-// };
+internals.ClientId._generateClientId = function(options) {
+    options = _.defaults(options || {}, internals.defaults);
 
-// internals.ClientId.create = function(options) {
-//     return internals.ClientId(options);
-// };
+    var possible = (options.letters ? internals.letters : '') + (options.numbers ? internals.numbers : '');
 
-// internals.ClientId.install = function() {
-//     if ( !localStorage.getItem(internals.localStorageKey)) {
-//         localStorage.setItem(internals.localStorageKey, internals.ClientId.create());
-//     }
-// };
+    return _.times(options.length, function() {
+        return possible.charAt(Math.floor(Math.random() * possible.length));
+    }).join('');
+};
 
-// internals.ClientId._store = {
-//     get: function() {
-
-//     },
-
-//     set: function() {
-
-//     }
-// }
-
-// internals.ClientId.defaults = internals.defaults;
-
-
-
-// function _generateClientId(options) {
-//     var possible = (options.letters ? internals.letters : '') + (options.numbers ? internals.numbers : '');
-
-//     return _.times(options.length, function() {
-//         return possible.charAt(Math.floor(Math.random() * possible.length));
-//     }).join('');
-// };
+internals.ClientId.defaults = internals.defaults;
