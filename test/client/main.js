54f71d5ff988c6ce2c8bcb9b4fdd4cc79da844c4
Michael Phillips <mlp.brooklyn@gmail.com>
2014-12-10 15:58:01 -0700

    Initial tests on settings view

--- test/client/main.js
+++ test/client/main.js
@@ -1,30 +1,31 @@
 require('es5-shim');
 
-require('./clientIdGenerator');
-require('./clipboard');
-require('./jQuerySnippet');
-require('./messageParser');
-require('./webSocketManager');
-require('./settingsStore');
-require('./app.js');
+// require('./clientIdGenerator');
+// require('./clipboard');
+// require('./jQuerySnippet');
+// require('./messageParser');
+// require('./webSocketManager');
+// require('./settingsStore');
+// require('./app.js');
 
 // describe blocks for test organization
 
 describe('Utilities', function() {
-    require('./utils/dateTimeFormatter');
-    require('./utils/handlebarsHelpers');
-    require('./utils/searchCriteria');
+    // require('./utils/dateTimeFormatter');
+    // require('./utils/handlebarsHelpers');
+    // require('./utils/searchCriteria');
 });
 
 describe('Backbone', function() {
     describe('Models', function() {
-        require('./models/request');
-        require('./models/settings');
+        // require('./models/request');
+        // require('./models/settings');
     });
     describe('Views', function() {
-        require('./views/channelSelector');
-        require('./views/request');
-        require('./views/serverLogs');
+        // require('./views/channelSelector');
+        // require('./views/request');
+        // require('./views/serverLogs');
+        require('./views/settings');
     });
 });
 
---
+++ test/client/views/settings.js
@@ -0,0 +1,46 @@
+// Load modules
+
+var Backbone = require('backbone');
+var SettingsView = require('../../../source/js/views/settings');
+
+// Declare internals
+
+var internals = {};
+
+
+
+
+internals.generateView = function(options) {
+    options = options || {};
+    options.settingsModel = options.settingsModel || new Backbone.Model();
+
+    return new SettingsView(options);
+}
+
+
+
+
+
+describe('SettingsView', function() {
+
+    describe('#template', function() {
+
+        it('returns html', function() {
+            var view = internals.generateView();
+
+            expect(view.template()).to.match(/<[a-z][\s\S]*>/); // html string
+        });
+
+    });
+
+    describe('.events', function() {
+
+        context('with a click on .submit', function() {
+
+
+
+        });
+
+    });
+
+});
