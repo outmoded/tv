654ec53b1cdc3b761d195461b01b60e2073bf249
Mike AbiEzzi <mjezzi@gmail.com>
2014-12-10 13:31:41 -0700

    Decompose method AppView#_renderChildView

--- source/js/views/app.js
+++ source/js/views/app.js
@@ -26,52 +26,77 @@ var AppView = Backbone.View.extend({
     },
 
     _renderChildViews: function($markup) {
-        var settingsView = new SettingsView({ 
-            el: $markup.siblings('.settings-modal-container'),
-            settingsModel: this.model
-        }).render();
-
-        var feedHeaderView = new FeedHeaderView({
-            el: $markup.find('.header'),
-            webSocketManager: this.webSocketManager
-        }).render();
-
-        var feedBodyView = new FeedBodyView({
-            el: $markup.find('.body'),
-            collection: this.collection
-        }).render();
-
-        var toolbarView = new ToolbarView({
-            el: $markup.siblings('.toolbar'),
-            model: this.model,
-            webSocketManager: this.webSocketManager
-        }).render();
+        var toolbarView =    this._renderToolbar($markup);
+        var settingsView =   this._renderSettings($markup);
+        var feedHeaderView = this._renderFeedHeader($markup);
+        var feedBodyView =   this._renderFeedBody($markup);
 
         this.listenTo(toolbarView, 'searchChanged', feedBodyView.filterRequests.bind(feedBodyView));
         this.listenTo(toolbarView, 'showSettings', settingsView.show);
         this.listenTo(toolbarView, 'clearFeed', function() {
-            feedHeaderView.clear();
-            feedBodyView.clear();
+            this._handleClearFeed(feedHeaderView, feedBodyView);
         });
 
         this.listenTo(feedHeaderView, 'toggleFavorites', feedBodyView.toggleFavorites.bind(feedBodyView));
         this.listenTo(feedHeaderView, 'collapseAll', feedBodyView.collapseAll.bind(feedBodyView));
 
         this.listenTo(feedBodyView, 'requestExpandToggle', function(expanded) {
-            if (!expanded && !feedBodyView.hasExpandedRequests()) {
-                feedHeaderView.disableCollapseAll();
-            } else if(expanded) {
-                feedHeaderView.enableCollapseAll();
-            }
+            this._handleRequestExpandToggle(expanded, feedHeaderView, feedBodyView);
         });
 
         this.listenTo(feedBodyView, 'requestFavoriteToggle', function(favorited) {
-            if (!favorited && !feedBodyView.hasFavoritedRequests()) {
-                feedHeaderView.disableFavoritesFilter();
-            } else if (favorited) {
-                feedHeaderView.enableFavoritesFilter();
-            }
+            this._handleRequestFavoriteToggle(favorited, feedHeaderView, feedBodyView);
         });
+    },
+
+    _renderToolbar: function($markup) {
+        return new ToolbarView({
+            el: $markup.siblings('.toolbar'),
+            model: this.model,
+            webSocketManager: this.webSocketManager
+        }).render();
+    },
+
+    _renderSettings: function($markup) {
+        return new SettingsView({
+            el: $markup.siblings('.settings-modal-container'),
+            settingsModel: this.model
+        }).render();
+    },
+
+    _renderFeedHeader: function($markup) {
+        return new FeedHeaderView({
+            el: $markup.find('.header'),
+            webSocketManager: this.webSocketManager
+        }).render();
+    },
+
+    _renderFeedBody: function($markup) {
+        return new FeedBodyView({
+            el: $markup.find('.body'),
+            collection: this.collection
+        }).render();
+    },
+
+    _handleRequestExpandToggle: function(expanded, feedHeaderView, feedBodyView) {
+        if (!expanded && !feedBodyView.hasExpandedRequests()) {
+            feedHeaderView.disableCollapseAll();
+        } else if(expanded) {
+            feedHeaderView.enableCollapseAll();
+        }
+    },
+
+    _handleRequestFavoriteToggle: function(favorited, feedHeaderView, feedBodyView) {
+        if (!favorited && !feedBodyView.hasFavoritedRequests()) {
+            feedHeaderView.disableFavoritesFilter();
+        } else if (favorited) {
+            feedHeaderView.enableFavoritesFilter();
+        }
+    },
+
+    _handleClearFeed: function(feedHeaderView, feedBodyView) {
+        feedHeaderView.clear();
+        feedBodyView.clear();
     }
 
 });
